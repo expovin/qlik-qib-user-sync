8d4b77910dd5febd413a3ec78b71738f74930e6f
Loïc Formont <loic.formont@gmail.com>
2019-07-11 19:39:08 +0200

    space issue 2

--- config.js
+++ config.js
@@ -2,19 +2,19 @@ const path = require('path');
 
 module.exports = {
     qrsApi: {
-		hostname: 'https://win2.qfp.qlik.com',
-		portNumber: '4242',
-		certificates: {
-			certFile: path.resolve(__dirname, 'certs', 'client.pem'),
-			keyFile: path.resolve(__dirname, 'certs', 'client_key.pem')
-		},
-	},
-	pg: {
-		hostname: 'localhost',
-		portNumber: '5432',
-		database: 'qlikinsightbot',
-		
-	},
+        hostname: 'https://win2.qfp.qlik.com',
+        portNumber: '4242',
+        certificates: {
+            certFile: path.resolve(__dirname, 'certs', 'client.pem'),
+            keyFile: path.resolve(__dirname, 'certs', 'client_key.pem')
+        },
+    },
+    pg: {
+        hostname: 'localhost',
+        portNumber: '5432',
+        database: 'qlikinsightbot',
+        
+    },
     getUserId: (userFull) => userFull.userId,
     getUserDirectory: (userFull) => userFull.userDirectory,
     getUserName: (userFull) => userFull.userId.split('@')[0].split('.').map((string) => string.charAt(0).toUpperCase() + string.slice(1)).join(' '),
