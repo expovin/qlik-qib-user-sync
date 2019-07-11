e25f00a66e1f8c6df890e85143b86ada72ba70c5
Loïc Formont <loic.formont@gmail.com>
2019-07-11 16:24:31 +0200

    testing pg connection

--- config.js
+++ config.js
@@ -1,15 +1,23 @@
 const path = require('path');
 
-export default {
-    hostname: 'https://localhost',
-    portNumber: '42420',
-    certificates: {
-        certFile: path.resolve(__dirname, 'certs', 'client.pem'),
-        keyFile: path.resolve(__dirname, 'certs', 'client_key.pem')
-    },
-    getUserId: (userFull) => userFull.id,
+module.exports = {
+    qrsApi: {
+		hostname: 'https://localhost',
+		portNumber: '4242',
+		certificates: {
+			certFile: path.resolve(__dirname, 'certs', 'client.pem'),
+			keyFile: path.resolve(__dirname, 'certs', 'client_key.pem')
+		},
+	},
+	pg: {
+		hostname: 'localhost',
+		portNumber: '5432',
+		database: 'qlikinsightbot',
+		
+	},
+    getUserId: (userFull) => userFull.name,
     getUserDirectory: (userFull) => userFull.userDirectory,
-    getUserName: (userFull) => userFull.name,
+    getUserName: (userFull) => userFull.name.split('@')[0].split('.').map((string) => string.charAt(0).toUpperCase() + string.slice(1)).join(' '),
     getUserEmail: (userFull) => userFull.name,
     userFilter: (userFull) => userFull.userDirectory === 'OKTA',
 }
--- index.js
+++ index.js
@@ -1,40 +1,65 @@
 const qrsInteract = require('qrs-interact');
-const config = require('./config');
 const extend = require('extend');
 
+const optionalRequire = require("optional-require")(require);
+
+const config = extend(
+	true,
+	{},
+	require('./config'),
+	optionalRequire('./config-secret') || {}
+);
+
 const { Pool  } = require('pg');
-const pool = new Pool()
 
-const qrs = new qrsInteract(extend({}, config));
+const pool = new Pool({
+	host: config.pg.hostname,
+	port: config.pg.portNumber,
+	database: config.pg.database,
+	user: config.pg.user,
+	password: config.pg.password,
+})
 
-pool.connect()
-    .then(client => {
+/*
+const qrs = new qrsInteract(extend({}, config.qrsApi));
+
         qrs
             .Get('user/full')
             .then((r) => r.body)
-            .then((users) => users.filter(user => user.userDirectory === 'OKTA'))
+			.then((users) => users.filter(user => config.userFilter(user)))
             .then((users) => users.map(user => ({
                 id: config.getUserId(user),
                 directory: config.getUserDirectory(user),
                 name: config.getUserName(user),
                 email: config.getUserEmail(user),
             })))
+			.then(console.log)
             .then((users) => {
+				return false;
+				
                 const text = 'SELECT id, name, email, directory FROM users'
 
-                return pool.query(text)
-                    .then((previousUsers) => previousUsers.map(previousUser => previousUser.id))
-                    .then((previousUsers) => users.filter(user => previousUsers.indexOf(user.id) > -1))
+                //return pool.query(text)
+                 //   .then((previousUsers) => previousUsers.map(previousUser => previousUser.id))
+                 //   .then((previousUsers) => users.filter(user => previousUsers.indexOf(user.id) > -1))
             })
-            .then((users) => Promise.all(users.map(user => {
-                const text = 'INSERT INTO users(name, email, directory) VALUES($1, $2, $3) RETURNING *'
-                const values = [user.name, user.userId, user.userDirectory]
+            .then((users) => {
+				return false;
+				
+				Promise.all(users.map(user => {
+					const text = 'INSERT INTO users(name, email, directory) VALUES($1, $2, $3) RETURNING *'
+					const values = [user.name, user.userId, user.userDirectory]
 
-                return pool.query(text, values);
-            })))
+					//return pool.query(text, values);
+				}))
+			})
             .then(console.log)
-            .finally(() => client.release())
+
+*/
+
+pool.connect()
+    .then(client => {
+		return pool.query("select table_name from information_schema.tables where table_schema = 'public'")
+			.then(console.log)
+			.finally(() => client.release())
     });
-
-
-
